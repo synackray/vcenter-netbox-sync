efae0970b900ae87fec70f4a77a3450bcc81878c
Raymond Beaudoin <24757919+synackray@users.noreply.github.com>
2019-12-08 01:49:26 +0100

    Created get functions for vCenter object types with common view function

--- run.py
+++ run.py
@@ -14,7 +14,7 @@ from logger import log
 def main():
     """Main function to run if script is called directly"""
     vc = vCenterHandler()
-    vc.get_hosts()
+    vc.view_explorer()
 
 class vCenterHandler:
     """Handles vCenter connection state and export of objects"""
@@ -37,7 +37,7 @@ class vCenterHandler:
             atexit.register(Disconnect, vc_instance)
             self.vc_content = vc_instance.RetrieveContent()
             log.info(
-                "Successfully authenticated to vCenter instance '%s'",
+                "Successfully authenticated to vCenter instance '%s'.",
                 settings.VC_HOST
                 )
         except (gaierror, vim.fault.InvalidLogin) as err:
@@ -48,20 +48,79 @@ class vCenterHandler:
                     settings.VC_HOST, settings.VC_PORT, err
                 ))
 
-    def get_hosts(self):
-        """Collect all hosts from vCenter"""
+    def create_view(self, obj_type):
+        """Create a view scoped to the vCenter object type desired.
+        This should be called by gets for vCenter object types.
+        """
+        # Mapping of object type keywords to view types
+        obj_types = {
+            "datacenters": vim.Datacenter,
+            "clusters": vim.ClusterComputeResource,
+            "hosts": vim.HostSystem,
+            "virtual_machines": vim.VirtualMachine,
+            }
         # Ensure an active vCenter session exists
         if not self.vc_content:
+            log.info("No existing vCenter session found.")
             self.authenticate()
-        host_view = self.vc_content.viewManager.CreateContainerView(
-            self.vc_content.rootFolder,
-            [vim.HostSystem],
-            True
+        return self.vc_content.viewManager.CreateContainerView(
+            self.vc_content.rootFolder, # View starting point
+            [obj_types[obj_type]], # Object types to look for
+            True # Should we recurively look into view
             )
-        obj = [host for host in host_view.view]
-        host_view.Destroy()
-        return obj
 
+    def get_datacenters(self):
+        """Collect all datacenters from vCenter"""
+        log.info("Collecting vCenter datacenter objects.")
+        container_view = self.create_view("datacenters")
+        datacenters = [dc.name for dc in container_view.view]
+        log.debug(
+            "%s datacenters were collected. The first datacenter is '%s'.",
+            len(datacenters), datacenters[0]
+            )
+        container_view.Destroy()
+        return datacenters
+
+    def get_hosts(self):
+        """Collect all hosts from vCenter"""
+        log.info("Collecting vCenter host objects.")
+        container_view = self.create_view("hosts")
+        hosts = [host for host in container_view.view]
+        log.debug(
+            "%s hosts were collected. The first host is '%s'.", len(hosts),
+            hosts[0].name
+            )
+        container_view.Destroy()
+        return hosts
+
+    def get_vms(self):
+        """Collect all Virtual Machines from vCenter"""
+        log.info("Collecting vCenter virtual machine objects.")
+        container_view = self.create_view("virtual_machines")
+        vms = [vm for vm in container_view.view]
+        log.debug(
+            "%s VMs were collected. The first VM is '%s'.", len(vms),
+            vms[0].name
+            )
+        container_view.Destroy()
+
+    def view_explorer(self):
+        """Interactive view explorer for exploring models"""
+        log.warning("View Explorer mode activated! Use with caution!")
+        obj_type = input("Object Type to Explore: ")
+        container_view = self.create_view(obj_type)
+        log.debug("Created view for object type '%s'.", obj_type)
+        objects = [obj for obj in container_view.view]
+        log.debug(
+            "Collected %s objects of object type %s.", len(objects), obj_type
+            )
+        log.warning(
+            "Entering interactive mode. Current data stored in 'objects' "
+            "variable."
+            )
+        import pdb
+        pdb.set_trace()
+        container_view.Destroy()
 
 if __name__ == "__main__":
     main()
